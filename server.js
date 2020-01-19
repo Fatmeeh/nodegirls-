bce32281b0c13169889c232f0f148a24ee78626a
Fatmeeh <lasheenfatmeeh@gmail.com>
2020-01-19 12:27:54 +0200

    step 3

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var http = require('http');
-var server=http.createServer();
-server.listen(3000,function(){
-    console.log('server on port 3000')
-})
+
+var message = 'I am so happy to be part of the Node Girls workshop!';
+
+function handler (request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message); 
+  response.end();
+
+}
+
+
+var server = http.createServer(handler);
+
+server.listen(3000, function () {
+
+    console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
